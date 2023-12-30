02046833af09c0600b46265d811f88c84a02c44e
Thirisha.A <123939975+thirisha3sha@users.noreply.github.com>
2023-12-30 16:20:22 +0530

    Add files via upload

---
+++ list slicing.py
@@ -0,0 +1,5 @@
+#Python program to display the first and last colors from the following list.
+col=input("enter the colrs:")
+col=col.split(" ")
+print("%s %s" %(col[0],col[-1]))
+#print(col[0],col[-1])
